1bb6bab35abc32f6f5a02ce01be6bc7f8c64ee90
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-05 15:06:52 +0700

    Update github.ts

--- common/constants/github.ts
+++ common/constants/github.ts
@@ -1,8 +1,8 @@
 export const GITHUB_ACCOUNTS = {
-  username: "satriabahari",
+  username: "nerusen",
   token: process.env.GITHUB_READ_USER_TOKEN_PERSONAL,
   endpoint: "/api/github",
   type: "personal",
-  github_url: "https://github.com/satriabahari",
+  github_url: "https://github.com/nerusen",
   is_active: true,
 };
