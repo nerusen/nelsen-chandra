8e9b24ff1ebaff75b3755350a4b53a5e78cf7696
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-05 16:41:49 +0700

    Update IntlToggle.tsx

--- common/components/layouts/sidebar/IntlToggle.tsx
+++ common/components/layouts/sidebar/IntlToggle.tsx
@@ -85,7 +85,7 @@ const IntlToggle = () => {
       >
         <motion.div
           transition={{ duration: 0.3, ease: "easeInOut" }}
-          className="flex h-8 w-8 items-center justify-center rounded-full bg-green-500 text-white"
+          className="flex h-8 w-8 items-center justify-center rounded-full bg-blue-300 text-white"
         >
           {locales[(currentIndex + 1) % locales.length].flag}
         </motion.div>
