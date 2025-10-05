c8cac662fbd44dce470a706aa40e68f92dfaff44
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-05 19:11:30 +0700

    Update MobileHeader.tsx

--- common/components/layouts/MobileHeader.tsx
+++ common/components/layouts/MobileHeader.tsx
@@ -36,7 +36,7 @@ const MobileHeader = () => {
           <div className="mt-1 flex items-center gap-2">
             <Link href="/" passHref>
               <h2 className="flex-grow whitespace-nowrap text-lg font-medium lg:text-xl">
-                Nelsen Chandra
+                n31sen.st
               </h2>
             </Link>
             <Tooltip title="Verified">
