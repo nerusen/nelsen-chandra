720d5e13141a0aef75e5279d4130da2598243a9c
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-13 09:34:57 +0800

    Add files via upload

--- common/components/layouts/sidebar/IntlToggle.tsx
+++ common/components/layouts/sidebar/IntlToggle.tsx
@@ -87,7 +87,7 @@ const IntlToggle = () => {
           transition={{ duration: 0.3, ease: "easeInOut" }}
           className="flex h-8 w-8 items-center justify-center rounded-full bg-neutral-300 text-neutral-900 dark:bg-neutral-700 dark:text-neutral-50 text-sm font-bold"
         >
-          {locales[(currentIndex + 1) % locales.length].flag}
+          {locales[currentIndex].flag}
         </motion.div>
       </button>
     </div>
