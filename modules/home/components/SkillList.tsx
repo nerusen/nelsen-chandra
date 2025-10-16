bb6296762f9bd94be16282f3ba4135669ff1552e
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-16 19:44:31 +0800

    Add files via upload

--- modules/home/components/SkillList.tsx
+++ modules/home/components/SkillList.tsx
@@ -29,7 +29,7 @@ const SkillList = () => {
         </SectionSubHeading>
       </div>
 
-      <div className="grid w-full gap-x-[1em] gap-y-[1.5em] py-2 grid-cols-4 md:grid-cols-10 lg:grid-cols-11 overflow-x-auto">
+      <div className="grid w-full gap-x-[0.5em] gap-y-[1.5em] py-2 grid-cols-3 md:grid-cols-10 lg:grid-cols-11">
         {stacksInArray.map(([name, { icon, background }], index) => (
           <div
             key={name}
