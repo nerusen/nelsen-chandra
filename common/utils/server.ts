2759262be2550c6d79ad7c58cae18d394b1dfbe1
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-17 19:25:41 +0800

    Add files via upload

--- common/utils/server.ts
+++ common/utils/server.ts
@@ -6,7 +6,7 @@ export const createClient = () => {
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       cookies: {
         getAll() {
@@ -27,5 +27,3 @@ export const createClient = () => {
     },
   );
 };
-};
-  );
