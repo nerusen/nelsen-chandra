adfc6a6efaa84dfcf7cc7eb8ef6e52ecd9251cf3
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-21 17:48:16 +0700

    Add files via upload

--- modules/music-room/components/MusicRoom.tsx
+++ modules/music-room/components/MusicRoom.tsx
@@ -188,7 +188,7 @@ const MusicRoom = () => {
         className="group flex w-fit items-center gap-2 rounded-lg border border-neutral-400 bg-neutral-100 px-3 py-2 text-sm transition duration-100 hover:text-neutral-800 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:text-neutral-200"
       >
         <FaSignOutAlt size={16} />
-        Sign Out
+        <span className="hidden sm:inline">Sign Out</span>
       </Button>
     </div>
   );
