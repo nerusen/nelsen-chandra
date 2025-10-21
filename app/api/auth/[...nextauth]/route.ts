0c3cea25e0367b5b3e52d5a60a27284db65f7237
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-21 12:14:41 +0700

    Fix GoogleProvider import and add SpotifyProvider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,21 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import GoolgeProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
+import SpotifyProvider from "next-auth/providers/spotify";
 
 const authOptions = {
   providers: [
-    GoolgeProvider({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID as string,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
