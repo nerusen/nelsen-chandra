f48a593f1ed49fb60324fd2fbe7eb713c3b46c70
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-21 15:39:42 +0700

    Add files via upload

--- common/components/elements/PageHeading.tsx
+++ common/components/elements/PageHeading.tsx
@@ -1,15 +1,27 @@
+import React from "react";
+
 interface PageHeadingProps {
   title?: string;
   description?: string;
+  children?: React.ReactNode;
 }
 
-const PageHeading = ({ title, description }: PageHeadingProps) => {
+const PageHeading = ({ title, description, children }: PageHeadingProps) => {
   return (
     <>
-      <h1 className="text-2xl font-medium">{title}</h1>
-      <p className="mb-6 border-b border-dashed  border-neutral-600 pb-6 pt-2 text-neutral-600 dark:text-neutral-400">
-        {description}
-      </p>
+      <div className="flex flex-col items-start justify-between gap-4 md:flex-row md:items-center">
+        <div>
+          <h1 className="text-2xl font-medium">{title}</h1>
+          <p className="mb-6 border-b border-dashed border-neutral-600 pb-6 pt-2 text-neutral-600 dark:text-neutral-400 md:mb-0 md:border-b-0 md:pb-0">
+            {description}
+          </p>
+        </div>
+        {children && (
+          <div className="flex items-center gap-4">
+            {children}
+          </div>
+        )}
+      </div>
     </>
   );
 };
