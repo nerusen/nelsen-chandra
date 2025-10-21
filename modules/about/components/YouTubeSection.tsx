3cc09179a4f9fd60d9b2a51bed3d506a5578ffab
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-21 09:26:51 +0700

    Add files via upload

--- modules/about/components/YouTubeSection.tsx
+++ modules/about/components/YouTubeSection.tsx
@@ -1,20 +1,15 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useRef } from "react";
 import { TbBrandYoutube as YouTubeIcon } from "react-icons/tb";
-
 import { LuExternalLink as ExternalLinkIcon } from "react-icons/lu";
 import { useTranslations } from "next-intl";
 
 import SectionHeading from "@/common/components/elements/SectionHeading";
 import SectionSubHeading from "@/common/components/elements/SectionSubHeading";
-import Button from "@/common/components/elements/Button";
 
 const YouTubeSection = () => {
   const t = useTranslations("AboutPage.youtube");
-  const [showOverlay, setShowOverlay] = useState(true);
-  const iframeRef = useRef<HTMLIFrameElement>(null);
 
   // YouTube video ID
   const videoId = "EwzWg-Joxq0";
@@ -46,26 +41,8 @@ const YouTubeSection = () => {
       </div>
 
       <div className="relative aspect-video w-full overflow-hidden rounded-lg bg-neutral-100 dark:bg-neutral-800">
-        {showOverlay && (
-          <div
-            className="absolute inset-0 z-10 flex cursor-pointer items-center justify-center bg-black bg-opacity-50"
-            onClick={() => setShowOverlay(false)}
-          >
-            <div className="flex h-20 w-20 items-center justify-center rounded-full bg-red-600">
-              <svg
-                className="ml-1 h-8 w-8 text-white"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M8 5v10l8-5-8-5z" />
-              </svg>
-            </div>
-          </div>
-        )}
-
         <iframe
-          ref={iframeRef}
-          src={`https://www.youtube.com/embed/${videoId}?enablejsapi=1&modestbranding=1&rel=0`}
+          src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&controls=0`}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
