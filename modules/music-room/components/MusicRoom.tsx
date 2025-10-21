e9b1549ee40afb9b48dc0e12f919da19b5ccffee
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-21 17:12:56 +0700

    Add files via upload

--- modules/music-room/components/MusicRoom.tsx
+++ modules/music-room/components/MusicRoom.tsx
@@ -185,7 +185,7 @@ const MusicRoom = () => {
       )}
       <Button
         onClick={handleLogout}
-        className="flex cursor-pointer items-center gap-1 rounded-md bg-red-600 px-3 py-1 font-medium text-red-50 transition duration-100 hover:bg-red-500 active:scale-90"
+        className="group flex w-fit items-center gap-2 rounded-lg border border-neutral-400 bg-neutral-100 px-3 py-2 text-sm transition duration-100 hover:text-neutral-800 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:text-neutral-200"
       >
         <FaSignOutAlt size={16} />
         Sign Out
