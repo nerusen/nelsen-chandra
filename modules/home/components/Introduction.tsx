baddee100f51770e991c2f1c0f75e04d4aee96c0
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-22 08:48:47 +0700

    Add files via upload

--- modules/home/components/Introduction.tsx
+++ modules/home/components/Introduction.tsx
@@ -18,7 +18,7 @@ const Introduction = () => {
           className="group flex w-fit items-center gap-2 rounded-lg border border-neutral-400 bg-neutral-100 px-3 py-2 text-sm transition duration-100 hover:text-neutral-800 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:text-neutral-200"
         >
           <SiBuymeacoffee />
-          <span className="hidden sm:inline">Buy me a coffee</span>
+          <span className="inline">Buy me a coffee</span>
         </Link>
       </div>
 
