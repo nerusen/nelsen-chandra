85341842b2e9f8ebdaf2d62b2282d3e08f58a16a
nerusen <162538024+nerusen@users.noreply.github.com>
2025-10-25 21:04:38 +0700

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,5 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoolgeProvider from "next-auth/providers/google";
-
-const authOptions = {
-  providers: [
-    GoolgeProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "@/common/libs/next-auth";
 
 const handler = NextAuth(authOptions);
 
