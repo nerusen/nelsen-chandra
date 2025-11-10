b35e00220de07d7d28c08f36aacf1f2be368046f
nerusen <162538024+nerusen@users.noreply.github.com>
2025-11-10 19:25:45 +0700

    Add files via upload

--- common/components/elements/PageHeading.tsx
+++ common/components/elements/PageHeading.tsx
@@ -12,7 +12,7 @@ const PageHeading = ({ title, description, children }: PageHeadingProps) => {
       <div className="flex flex-col items-start justify-between gap-4 md:flex-row md:items-center">
         <div>
           <h1 className="text-2xl font-medium">{title}</h1>
-          <p className="mb-6 border-b border-dashed border-neutral-600 pb-6 pt-2 text-neutral-600 dark:text-neutral-400 md:mb-0 md:border-b-0 md:pb-0">
+          <p className="mb-6 border-b border-dashed border-neutral-600 pb-6 pt-2 text-neutral-600 dark:text-neutral-400">
             {description}
           </p>
         </div>
