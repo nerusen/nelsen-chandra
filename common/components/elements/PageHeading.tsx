3a3f1209711fe950c86189dcf7dac369ebeddf68
nerusen <162538024+nerusen@users.noreply.github.com>
2025-11-10 21:51:04 +0700

    Add files via upload

--- common/components/elements/PageHeading.tsx
+++ common/components/elements/PageHeading.tsx
@@ -9,18 +9,21 @@ interface PageHeadingProps {
 const PageHeading = ({ title, description, children }: PageHeadingProps) => {
   return (
     <>
-      <div className="flex flex-col items-start justify-between gap-4 md:flex-row md:items-center">
-        <div>
-          <h1 className="text-2xl font-medium">{title}</h1>
-          <p className="mb-6 border-b border-dashed border-neutral-600 pb-6 pt-2 text-neutral-600 dark:text-neutral-400">
-            {description}
-          </p>
-        </div>
-        {children && (
-          <div className="flex items-center gap-4">
-            {children}
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col items-start justify-between gap-4 md:flex-row md:items-center">
+          <div>
+            <h1 className="text-2xl font-medium">{title}</h1>
+            <p className="pt-2 text-neutral-600 dark:text-neutral-400">
+              {description}
+            </p>
           </div>
-        )}
+          {children && (
+            <div className="flex items-center gap-4">
+              {children}
+            </div>
+          )}
+        </div>
+        <div className="border-b border-dashed border-neutral-600"></div>
       </div>
     </>
   );
