6d04aedc89c5ec62ae4dde8540f79e7d4bfe44d6
nerusen <162538024+nerusen@users.noreply.github.com>
2025-11-11 08:42:51 +0700

    Add files via upload

--- modules/chat/components/ChatItemSkeleton.tsx
+++ modules/chat/components/ChatItemSkeleton.tsx
@@ -4,14 +4,42 @@ import Skeleton from "react-loading-skeleton";
 const ChatItemSkeleton = () => {
   return (
     <SkeletonLoader>
-      <div className="flex gap-4">
-        <Skeleton className="h-full" circle containerClassName="w-[60px]" />
-        <div className="w-full">
-          <div className="flex w-1/2 gap-4">
-            <Skeleton containerClassName="w-full" />
-            <Skeleton containerClassName="w-full" />
+      <div className="space-y-5 py-4">
+        {/* Skeleton 1 */}
+        <div className="flex items-center gap-3 px-4 lg:px-8">
+          <Skeleton className="h-full" circle containerClassName="w-[40px]" />
+          <div className="space-y-1 flex-1">
+            <div className="flex flex-col md:flex-row md:items-center gap-x-2 gap-y-1">
+              <Skeleton containerClassName="w-20" />
+            </div>
+            <div className="group flex flex-col w-fit relative">
+              <Skeleton containerClassName="w-64" className="h-12 rounded-xl" />
+            </div>
           </div>
-          <Skeleton containerClassName="w-full" className="h-6" />
+        </div>
+        {/* Skeleton 2 */}
+        <div className="flex items-center gap-3 px-4 lg:px-8 flex-row-reverse">
+          <Skeleton className="h-full" circle containerClassName="w-[40px]" />
+          <div className="space-y-1 flex-1 flex flex-col items-end">
+            <div className="flex flex-col md:flex-row md:items-center gap-x-2 gap-y-1 md:flex-row-reverse">
+              <Skeleton containerClassName="w-20" />
+            </div>
+            <div className="group flex flex-col w-fit relative items-end">
+              <Skeleton containerClassName="w-48" className="h-10 rounded-xl" />
+            </div>
+          </div>
+        </div>
+        {/* Skeleton 3 */}
+        <div className="flex items-center gap-3 px-4 lg:px-8">
+          <Skeleton className="h-full" circle containerClassName="w-[40px]" />
+          <div className="space-y-1 flex-1">
+            <div className="flex flex-col md:flex-row md:items-center gap-x-2 gap-y-1">
+              <Skeleton containerClassName="w-16" />
+            </div>
+            <div className="group flex flex-col w-fit relative">
+              <Skeleton containerClassName="w-56" className="h-14 rounded-xl" />
+            </div>
+          </div>
         </div>
       </div>
     </SkeletonLoader>
