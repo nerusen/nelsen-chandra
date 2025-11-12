7b554af89ab664b2d5f0ee16164dcb6c9fa4b98f
nerusen <162538024+nerusen@users.noreply.github.com>
2025-11-12 11:45:42 +0700

    Change profile image source from .png to .jpg

--- common/components/layouts/MobileHeader.tsx
+++ common/components/layouts/MobileHeader.tsx
@@ -26,7 +26,7 @@ const MobileHeader = () => {
         >
           <div className="z-10 w-max rounded-full google-profile-border shadow-md">
             <Image
-              src={"/images/nelsen.png"}
+              src={"/images/nelsen.jpg"}
               alt="profile"
               width={isOpen ? 80 : imageSize * 0.9}
               height={isOpen ? 80 : imageSize * 0.9}
